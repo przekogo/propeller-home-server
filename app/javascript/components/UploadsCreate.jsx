25e32e803462f29fdd2e878641e010a9ed23dd49
Kogo <przeroog@gmail.com>
2021-07-04 21:58:13 +0200

    multiple upload handling + upload model attachment validation
    next step - implement proper progress bars with redux

--- app/javascript/components/UploadsCreate.jsx
+++ app/javascript/components/UploadsCreate.jsx
@@ -1,33 +1,47 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class UploadsCreate extends Component {
   constructor(props,
   ) {
     super(props);
+    this.state = {
+      // TODO replace with redux
+      upload_progress: Number,
+    };
     this.handleUploadChange = this.handleUploadChange.bind(this);
   }
 
   handleUploadChange(event) {
-    const body = new FormData();
-    body.append('upload[file]', event.target.files[0]);
-    const url = 'api/v1/uploads';
-    const options = {
-      method: 'POST',
-      body: body,
-    };
-    fetch(url, options)
-        .then((response) => response.json())
-        .then(() => {
-          this.props.parentCallback();
-        });
+    Array.from(event.target.files).forEach(async (file) => {
+      const payload = new FormData();
+      payload.append('upload[file]', file);
+      event.target.value = null;
+
+      await axios({
+        url: 'api/v1/uploads',
+        method: 'post',
+        data: payload,
+        onUploadProgress: (progress) => {
+          const {loaded, total} = progress;
+          const percentageProgress = Math.floor((loaded/total) * 100);
+          this.setState({upload_progress: percentageProgress});
+        },
+      });
+      this.props.parentCallback();
+      this.setState({upload_progress: null});
+    });
   }
 
   render() {
     return (
-      <div className='upload-form'>
-        <label>Upload</label>
-        <input type="file" onChange={this.handleUploadChange} />
-      </div>
+      <>
+        <h1>{this.state.upload_progress}</h1>
+        <div className='upload-form'>
+          <label>Upload</label>
+          <input type="file" multiple onChange={this.handleUploadChange} />
+        </div>
+      </>
     );
   }
 }
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,4 +2,12 @@
 
 class Upload < ApplicationRecord
   has_one_attached :file
+
+  validate :file_has_name
+
+  private
+
+  def file_has_name
+    errors.add(:file_name, 'file name cannot be blank') unless file.filename.present?
+  end
 end
